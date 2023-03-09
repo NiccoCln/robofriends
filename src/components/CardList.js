4e33386c4d9fc3c797b1ed860330b30e610a7446
Nicoleta Caluian <nicco_cln@yahoo.com>
2023-03-09 20:22:47 +0200

    adding files of project

---
+++ src/components/CardList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({robots}) => {
+	const cardsArray = robots.map((robot, i) => {
+		return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+	})
+	if (!robots) {
+		throw new Error();
+	}
+	return (
+		<div>
+			{cardsArray}
+		</div>
+	)
+}
+
+export default CardList;
